a5d112589bb01bc38c51518432bf5d6ea455fb00
ks2355244 <ks2355244@yahoo.com>
2016-06-09 11:14:18 +0800

    1st commit

--- prime-factors.rb
+++ prime-factors.rb
@@ -1,1 +1,14 @@
 # Place your solutions here
+def my_wanted_number(n)
+	if n < 2:
+		return []
+		prime_factors = []
+		for p in prime_sieve (int(n ** 0.5))
+if p*p > n: break
+while n % p == 0:
+prime_factors.append(p)
+n // = p
+if n > 1:
+	prime_factors.append(n) 
+	return prime_factors 
+end
